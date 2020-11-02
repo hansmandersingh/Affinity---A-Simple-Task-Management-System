41930df4d6e62c580ff3b4982fa5d42c087e5955
Hansmander <hansmandersingh1998@gmail.com>
2020-11-02 10:12:03 -0600

    UserManager Class Added

---
+++ Affinity/Controllers/UserManagerController.cs
@@ -0,0 +1,25 @@
+﻿using Affinity.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Affinity.Controllers
+{
+    public class UserManagerController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: UserManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void AddUser(string email, string pwdHash)
+        {
+            
+        }
+    }
+}
