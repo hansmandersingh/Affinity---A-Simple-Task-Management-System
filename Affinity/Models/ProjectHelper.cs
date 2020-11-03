b86fecfde2a7797b7948057fc08da669bc0958fb
Hansmander <hansmandersingh1998@gmail.com>
2020-11-03 13:22:38 -0600

    Important helper funcs added into project helper

--- Affinity/Models/ProjectHelper.cs
+++ Affinity/Models/ProjectHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +11,47 @@ namespace Affinity.Models
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        
+
+        public void CreateProject(string Name, string Description)
+        {
+            Project project = new Project() { Name = Name, Description = Description };
+
+            db.Projects.Add(project);
+            db.SaveChanges();
+        }
+
+        public void UpdateProject(Project pro)
+        {
+            Project project = db.Projects.Find(pro.Id);
+
+            if (project != null)
+            {
+                project.Name = pro.Name;
+                project.Description = pro.Description;
+                project.Tasks = pro.Tasks;
+                db.Entry(project).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteProject(int id)
+        {
+            Project project = db.Projects.Find(id);
+
+            if(project != null)
+            {
+                db.Projects.Remove(project);
+                db.SaveChanges();
+            }
+        }
+        public List<Project> GetAllProjects()
+        {
+            return db.Projects.ToList();
+        }
+
+        public Project GetAProject(int id)
+        {
+            return db.Projects.Find(id);
+        }
     }
 }
