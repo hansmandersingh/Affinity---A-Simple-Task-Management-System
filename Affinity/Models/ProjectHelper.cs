e87fa8167f57118a0ff451da3890309c1e3f32b5
Hansmander <hansmandersingh1998@gmail.com>
2020-11-03 13:02:06 -0600

    Initial ProjectHelper file created

---
+++ Affinity/Models/ProjectHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Affinity.Models
+{
+    
+    public class ProjectHelper
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        
+    }
+}
