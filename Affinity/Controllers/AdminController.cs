ba562e9634805494aff13c238123682f773ffa58
Hansmander <hansmandersingh1998@gmail.com>
2020-11-04 09:48:39 -0600

    Added a new Admin Controller to see users and create roles

---
+++ Affinity/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Affinity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Affinity.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
