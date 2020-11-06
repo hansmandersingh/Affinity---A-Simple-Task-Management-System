4c9cbcfa6e5f09cc00a7ce63e84595b01a6514f7
Hansmander <hansmandersingh1998@gmail.com>
2020-11-05 20:45:00 -0600

    Small Changes made to application user class

--- Affinity/Models/IdentityModels.cs
+++ Affinity/Models/IdentityModels.cs
@@ -11,7 +11,7 @@ namespace Affinity.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public ICollection<Task> Tasks;
+        public ICollection<Task> Tasks { get; set; }
 
         public ApplicationUser()
         {
