42a2af012397ca8b7bf04618d9fe1fc9b505a8fa
Hansmander <hansmandersingh1998@gmail.com>
2020-11-05 19:45:28 -0600

    can view all the tasks from developer side

---
+++ Affinity/Controllers/DeveloperController.cs
@@ -0,0 +1,29 @@
+﻿using Affinity.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Affinity.Controllers
+{
+    [Authorize(Roles = "developer")]
+    public class DeveloperController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Developer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAllTasks()
+        {
+            var allTasks = TaskHelper.GetAllTasksByADeveloper(this.User.Identity.GetUserId());
+            return View(allTasks);
+        }
+
+        
+    }
+}
