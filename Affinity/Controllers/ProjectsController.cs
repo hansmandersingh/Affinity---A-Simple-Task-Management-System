4186fedc6d8ff6d098e64ef8bd74a7e507ef6231
Hansmander <hansmandersingh1998@gmail.com>
2020-11-07 16:08:45 -0600

    Added Priority feature to Project and updated views according to that

--- Affinity/Controllers/ProjectsController.cs
+++ Affinity/Controllers/ProjectsController.cs
@@ -46,7 +46,7 @@ namespace Affinity.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,Description,Time,IsCompleted")] Project project)
+        public ActionResult Create([Bind(Include = "Id,Name,Description,Time,IsCompleted, Priority")] Project project)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace Affinity.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Description,Time,IsCompleted")] Project project)
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,Time,IsCompleted,Priority")] Project project)
         {
             if (ModelState.IsValid)
             {
