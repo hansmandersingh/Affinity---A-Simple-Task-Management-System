96ad716a9304005293a409b681014c783af8fc89
Navdeepsin <navdeepsin31@gmail.com>
2020-11-07 20:40:41 -0600

    changes

---
+++ Affinity/Controllers/ProjectManagerController.cs
@@ -0,0 +1,20 @@
+﻿using Affinity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Affinity.Controllers
+{
+    [Authorize(Roles = "project manager")]
+    public class ProjectManagerController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: ProjectManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
