afeba6ff6be1da09e90b644557d76dcad3e7aa5d
Navdeepsin <navdeepsin31@gmail.com>
2020-11-10 10:49:44 -0600

    adding project manager level permissions

--- Affinity/Controllers/ProjectManagerController.cs
+++ Affinity/Controllers/ProjectManagerController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.DynamicData;
 using System.Web.Mvc;
 
 namespace Affinity.Controllers
@@ -10,11 +11,27 @@ namespace Affinity.Controllers
     [Authorize(Roles = "project manager")]
     public class ProjectManagerController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private static ApplicationDbContext db = new ApplicationDbContext();
         // GET: ProjectManager
         public ActionResult Index()
         {
-            return View();
+            ICollection<Project> allProjects;
+            allProjects = ProjectHelper.GetAllProjects();
+            return View(allProjects);
+        }
+
+        public ActionResult GetAllTasksByProject(int proId)
+        {
+            ICollection<Task> allTasks;
+            allTasks = TaskHelper.getAllTasks();
+            var allTasksinDesc = allTasks.Where(t => t.ProjectId == proId).OrderByDescending(i => i.CompletedPercentage).ToList();
+            return View(allTasksinDesc);
+        }
+
+        public ActionResult HideCompletedTask()
+        {
+            var allTasksWithoutHidden = TaskHelper.getAllTasks();
+            return View(allTasksWithoutHidden);
         }
     }
 }
